951e585a9ec91daa4305b7ee084e7a132e329bcd
I554012 <t.domagala@sap.com>
2024-02-17 10:46:47 +0100

    codewars VowelCount

--- kyu8/AbbreviateTwoWords.java
+++ kyu8/AbbreviateTwoWords.java
@@ -1,12 +1,17 @@
 package kyu8;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+/*
+* Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+* */
 public class AbbreviateTwoWords
 {
 	public static String abbrevName(final String name) {
-		final String[] firstname = name.split(" ", 1);
-		System.out.println(firstname);
-		return name;
+		return name.split(" ")[0].substring(0,1).toUpperCase()+"."+name.split(" ")[1].substring(0,1).toUpperCase();
 	}
 
 	@Test
